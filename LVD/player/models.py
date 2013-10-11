7b646aabe7b78a47beeffd3542d8f2705dbbf6f2
alice <xlai@suse.com>
2013-10-11 12:27:23 +0800

    alice add database

--- LVD/player/models.py
+++ LVD/player/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+    sex = models.CharField(max_length=6)
+    birth = models.DateField()
+    country = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
+
+class VideoIntroduction(models.Model):
+    director = models.CharField(max_length=30)
+    actors = models.ManyToManyField(Actor)
+    content = models.CharField(max_length=500)
+
+class Classification(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class VideoInfo(models.Model):
+    name = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
+    score = models.CharField(max_length=3) # range 0 to 100
+    releaseDate = models.DateField()
+    thumbnailsLoc = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    classification = models.ManyToManyField(Classification)
+    introduction = models.ForeignKey(VideoIntroduction)
+
+
--- LVD/player/views.py
+++ LVD/player/views.py
@@ -13,3 +13,27 @@ def player(request,video):
     ids = (2,3,4,5,6,7,8)
     data=zip(ids,clips)	
     return render_to_response('player.html', locals(), context_instance=RequestContext(request))
+
+#def current_datetime(request):
+#    now = datetime.datetime.now()
+#    return render_to_response('current_datetime.html', {'current_date': now})
+#def showTablePublisher(request):
+    #p1 = Publisher(name='Apress', address='2855 Telegraph Avenue',city='Berkeley', state_province='CA', country='U.S.A.',website='http://www.apress.com/')
+    #p1.save()
+    #p2 = Publisher(name="O'Reilly", address='10 Fawcett St.',city='Cambridge', state_province='MA', country='U.S.A.', website='http://www.oreilly.com/')
+    #p2.save()
+#    publisher_list = Publisher.objects.all()
+
+
+
+def initDB:
+    pass
+
+def insertVideo():
+    pass
+
+# work way 
+#>>> b1= Book(title='my book 1', publisher=p1,publication_date='2013-09-20')
+#>>> b1.save()
+#>>> b1.authors.add(a1,a2);
+                               
